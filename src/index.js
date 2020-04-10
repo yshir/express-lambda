fe4058b7a33935a26f2a4805e741ecdec2f4d96f
yshir <yshir0020@gmail.com>
2020-04-10 22:12:47 +0900

    install express

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, world.' })
+})
+
+app.get('/users', (req, res) => {
+  res.json({ data: [{ id: 1, name: 'test' }] })
+})
+
+const port = process.env.PORT || '8080'
+app.listen(port, () => {
+  console.log(`🚀 Server running\n> http://localhost:${port}`)
+})
